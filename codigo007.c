8c776b8f6cc20444247c21c19a988822452094c2
Yuri-martins-dev <yurimartins.dev@yahoo.com>
2026-02-19 21:23:53 -0300

    Beecrowd 1006: Média Ponderada

---
+++ codigo007.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    double a, b, c, media;
+    scanf(" %lf %lf %lf", &a, &b, &c);
+
+    a = a * 2.0;
+    b = b * 3.0;
+    c = c * 5.0;
+    media = (a + b + c)/10.0;
+    printf("MEDIA = %.1lf\n", media);
+
+    return 0;
+}
